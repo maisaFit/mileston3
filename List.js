897f59ab9677db7af818bad1c543822afb1c3877
maisaFit <129098319+maisaFit@users.noreply.github.com>
2023-04-23 00:49:01 -0400

    Create List.js

---
+++ List.js
@@ -0,0 +1,12 @@
+# mileston3import Nav from "./Nav.js";
+const  ContactUs =()=> {
+    return(
+ 
+     <>
+     <Nav></Nav>
+     contact us
+     hello
+     </>
+    );
+};
+export default ContactUs;
