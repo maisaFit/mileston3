d06f31287a339a13e72109ebb9166c3daf166320
maisaFit <129098319+maisaFit@users.noreply.github.com>
2023-04-23 00:50:43 -0400

    Create Nav.js

---
+++ Nav.js
@@ -0,0 +1,21 @@
+# mileston3import {Link} from "react-router-dom";
+const Nav=()=> {
+    return(
+        <div><header>
+        <nav>
+            
+                <Link to="/"> My To-Do-List <br/>
+                </Link>
+                <Link to="ContactUs"> contact us <br/>
+                </Link>
+                <Link to="ViewDetails"> View the Details of the task <br/>
+                </Link>
+                <Link to="CreatTask"> Creat a new Task <br/>
+                 </Link>
+            </nav>
+        </header>
+        </div>
+    );
+};
+export default Nav;
+     
